bb81835022007f75fa899466d7f560204fa61a9b
bliff182 <michael.bither@gmail.com>
2019-10-21 13:59:25 -0400

    added buttons work, able to start/stop gifs

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,19 +1,28 @@
 $(document).ready(function () {
 
+    // VARIABLES
+    // ============================================================================================================
+
+
     // initial movies to be displayed as buttons
     var topics = ["The Shawshank Redemption", "The Godfather", "The Godfather: Part II", "The Dark Knight", "12 Angry Men",
-        "Schindler's List", "The Lord of the Rings: The Return of the King", "Pulp Fiction"/*, "The Good, the Bad, and the Ugly", 
-    "Fight Club", "The Lord of the Rings: The Fellowship of the Ring", "Forrest Gump", "Joker", "Inception", "The Empire Strikes Back", 
-    "The Lord of the Rings: The Two Towers", "The Matrix", "One Flew Over the Cuckoo's Nest", "Goodfellas", "Seven Samurai" */];
+        "Schindler's List", "The Lord of the Rings: The Return of the King", "Pulp Fiction"];
+
+
+    // FUNCTIONS
+    // ============================================================================================================
+
 
     // create buttons dynamically
     function createButtons() {
         $(".button-list").empty(); // prevent repeat buttons on each submission
         for (var i = 0; i < topics.length; i++) {
+
             var topicButton = $("<button>");
             topicButton.addClass("btn btn-info topic-btn");
             topicButton.attr("topic-data", topics[i]);
             topicButton.text(topics[i]);
+
             $(".button-list").append(topicButton);
         }
     }
@@ -22,26 +31,32 @@ $(document).ready(function () {
     function displayGifs() {
 
         var selection = $(this).attr("topic-data");
-        var queryUrl = "http://api.giphy.com/v1/gifs/search?q=" + selection + "&api_key=TxtD69JyrzOXwm3BZ7B0RU36mk5OTuuZ&limit=10";
+        var queryUrl = "https://api.giphy.com/v1/gifs/search?q=" + selection + "&api_key=TxtD69JyrzOXwm3BZ7B0RU36mk5OTuuZ&limit=10";
 
         $.ajax({
             url: queryUrl,
             method: "GET"
-        }).then(function(response) {
+        }).then(function (response) {
 
             console.log(response);
-
             var results = response.data;
 
             for (var i = 0; i < results.length; i++) {
+
                 var gifDiv = $("<div>");
+                gifDiv.addClass("gif-data");
+
                 var gifRating = $("<p>").text("Rating: " + results[i].rating);
-                // console.log(results[i].rating);
+
                 var gifImage = $("<img>");
                 gifImage.attr("src", results[i].images.fixed_height_still.url);
+                gifImage.attr("data-still", results[i].images.fixed_height_still.url);
+                gifImage.attr("data-animate", results[i].images.fixed_height.url);
+                gifImage.attr("data-state", "still");
+                gifImage.addClass("gif");
 
-                gifDiv.append(gifRating);
                 gifDiv.append(gifImage);
+                gifDiv.append(gifRating);
 
                 $("#gif-display").prepend(gifDiv);
             }
@@ -49,15 +64,43 @@ $(document).ready(function () {
         });
     }
 
+    // toggle gifs between still and animated
+    function toggleAnimation() {
+
+        var state = $(this).attr("data-state");
+
+        // console.log(state);
+
+        if (state === "still") {
+            $(this).attr("src", $(this).attr("data-animate"));
+            $(this).attr("data-state", "animate");
+        }
+        else {
+            $(this).attr("src", $(this).attr("data-still"));
+            $(this).attr("data-state", "still");
+        }
+    }
+
+
+    // FUNCTION EXECUTION
+    // ============================================================================================================
+
+
+    // create initial set of buttons
+    createButtons();
+
     // add buttons from search bar
-    $("#movie-search").click(function () {
+    $("#topic-search").on("click", function () {
         event.preventDefault();
-        var searchTerm = $("#movie-text").val().trim(); // store search term in movie variable
+        var searchTerm = $("#topic-text").val().trim(); // store search term in variable
         topics.push(searchTerm);
         createButtons();
     })
 
-    createButtons();
-    $(".topic-btn").on("click", displayGifs);
+    // click button to display gifs
+    $(document).on("click", ".topic-btn", displayGifs);
+
+    // click gif to toggle animation on or off
+    $(document).on("click", ".gif", toggleAnimation);
 
 });
